d8863d1825817442455b9379a1686ab4000f61b1
Sergiod3x <sergiod3x.@gmail.com>
2021-09-01 16:28:35 +0200

    Ok Bonus

---
+++ resources/views/paperino.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> Io sono {{$name}} </h1>
+
+    <img src="{{$srcLogo}}" />
+
+</body>
+</html>
+
+<!-- Styles -->
+<style>
+            img {
+            
+                height: 200px;
+                width : auto;
+            }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pietro', function () {
+    return view('pietro',
+        [
+            'name' => 'Pietro',
+            'srcLogo' => 'https://static.miraheze.org/nonciclopediawiki/a/a9/Pietro_Gambadilegno.gif'
+        ]
+    );
+});
+
+Route::get('/pluto', function () {
+    return view('pluto',
+        [
+            'name' => 'Pluto',
+            'srcLogo' => 'https://i0.wp.com/www.ventennipaperoni.com/wp-content/uploads/2020/05/pluto.jpg?resize=512%2C288&ssl=1'
+        ]
+    );
+});
+
+Route::get('/topolino', function () {
+    return view('topolino',
+        [
+            'name' => 'Topolino',
+            'srcLogo' => 'https://www.tgtourism.tv/wp-content/uploads/2018/03/topolino.jpg'
+        ]
+    );
+});
+
+Route::get('/pippo', function () {
+    return view('pippo',
+        [
+            'name' => 'Pippo',
+            'srcLogo' => 'https://www.topolino.it/wp-content/uploads/2019/12/pippointera.png'
+        ]
+    );
+});
+
+Route::get('/minnie', function () {
+    return view('minnie',
+        [
+            'name' => 'Minnie',
+            'srcLogo' => 'https://www.shop.event-planner.it/wp-content/uploads/2020/07/7752f59d6892ab81b24fb3c85d0355b1.jpg'
+        ]
+    );
+});
